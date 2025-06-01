1ba7870fafd3e173332eb36ccd29212742a61896
Jerry Mathew <jer2ymatheww@gmail.com>
2025-06-01 22:48:58 +0530

    movie-card

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,11 @@
 import { useEffect } from "react";
 import Navbar from "../../components/Navbar";
+import MovieCard from "../../components/MovieCard";
 import { useDispatch, useSelector } from "react-redux";
 import  getMovies  from "../../api/movies";
+import Grid from '@mui/material/Grid';
 
+import Box from '@mui/material/Box';
 function Home() {
   const dispatch = useDispatch();
   const { movies } = useSelector((state) => state.movies);
@@ -14,6 +17,13 @@ function Home() {
   return (
     <>
       <Navbar />
+      <Box sx={{ flexGrow: 1, padding: 5}}>
+        <Grid container spacing={5}>
+        {
+          movies?.length > 0 && movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)
+        }
+        </Grid>
+      </Box>
     </>
   );
 }
